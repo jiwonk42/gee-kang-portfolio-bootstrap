ce6e8e2cdece67da30ab2026b4091e603899e902
Jiwon Kang <jiwonk42@gmail.com>
2017-05-12 16:18:05 -0700

    add a background cover video

---
+++ GeeKang/Context/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GeeKang.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GeeKang.Context
+{
+    public class ApplicationDataContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDataContext()
+            : base("DefaultConnection")
+        { }
+
+        public System.Data.Entity.DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ GeeKang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GeeKang.Startup))]
+namespace GeeKang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
