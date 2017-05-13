abdc6d20090e7cbcbba51fe6c0b66c7c846089ff
Jiwon Kang <jiwonk42@gmail.com>
2017-05-12 18:32:44 -0700

    add a ProjectController and Project Index page

---
+++ GeeKang/Controllers/ProjectController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeeKang.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
